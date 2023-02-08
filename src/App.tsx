f5e301737d48d8504d2c8f27cdc3ed7f30b4c569
Fernando Ojeda <fojeda@novut.com>
2023-02-08 04:22:35 -0600

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -57,16 +57,6 @@ function App() {
 
                                             <div className="flex items-center flex-shrink-0 h-10 px-2">
                                                 <span className="block text-sm font-semibold">{stages[status]}</span>
-                                                {/*
-                                <span className="flex items-center justify-center w-5 h-5 ml-2 text-sm font-semibold text-indigo-500 bg-white rounded bg-opacity-30">{tasks.length}</span>
-
-
-                                <button className="flex items-center justify-center w-6 h-6 ml-auto text-indigo-500 rounded hover:bg-indigo-500 hover:text-indigo-100">
-                                    <svg className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
-                                    </svg>
-                                </button>
-                                */}
                                             </div>
 
 
